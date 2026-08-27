81b00636518ea69bb1818e62eb7dee04cd468a95
soomin <riato101@naver.com>
2026-08-27 10:13:46 +0900

    다음 블록, 다다음 블록 미리보게 뜨게 함

--- board.js
+++ board.js
@@ -1,11 +1,8 @@
 const cols = 10;
 const rows = 20;
-
 const createEmptyGrid = () => {
     const board = Array.from({length: rows}, () => Array(cols).fill(null));
 
     return board;
 };
-
 const grid = createEmptyGrid();
-
--- main.js
+++ main.js
@@ -2,12 +2,33 @@ const canvas = document.getElementById('board_canvas');
 const cellSize = 20;
 const renderer = new Renderer(canvas, cellSize);
 
+// 블록 미리보기 렌더러
+const previewCanvas = document.getElementById('next_preview');
+const previewCellSize = 10;
+const previewRenderer = new Renderer(previewCanvas, previewCellSize);
+
+const subPreviewCanvas = document.getElementById('sub_next_preview');
+const subPreviewCellSize = 5;
+const subPreviewRenderer = new Renderer(subPreviewCanvas, subPreviewCellSize);
+
 function draw() {
     renderer.clear();
     renderer.drawGrid(grid);
     renderer.drawPiece({
         cells: cellsAbsolutePosition(currentPiece),
         color: currentPiece.color,
+    });
+
+    previewRenderer.clear();
+    previewRenderer.drawPiece({
+        cells: q.queue[0].cells,
+        color: q.queue[0].color,
+    });
+
+    subPreviewRenderer.clear();
+    subPreviewRenderer.drawPiece({
+        cells: q.queue[1].cells,
+        color: q.queue[1].color,
     });
 }
 
