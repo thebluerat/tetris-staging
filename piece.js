729ffea86dfaa82026ef1f3a155b26512f456770
soomin <riato101@naver.com>
2026-08-28 17:00:47 +0900

    블록 스폰 위치 조정

--- piece.js
+++ piece.js
@@ -1,7 +1,7 @@
 const PIECES = {
-    I: {color : 'skyblue', cells: [[0,1], [1,1], [2,1], [3,1]]},
-    O: {color : 'yellow', cells: [[1,1], [1,2], [2,1], [2,2]]},
-    T: {color : 'purple', cells: [[0,0], [0,1], [0,2], [1,1]]},
+    I: {color : 'skyblue', cells: [[0,0], [1,0], [2,0], [3,0]]},
+    O: {color : 'yellow', cells: [[1,0], [1,1], [2,0], [2,1]]},
+    T: {color : 'purple', cells: [[1,0], [1,1], [1,2], [2,1]]},
     S: {color : 'green', cells: [[0,1], [1,1], [1,0], [2,0]]},
     Z: {color : 'red', cells: [[1,0], [2,0], [2,1], [3,1]]},
     J: {color : 'blue', cells: [[2,0], [2,1], [2,2], [1,2]]},
