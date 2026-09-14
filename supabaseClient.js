a7de65ad0e0f3df084e532f9ee1f00a0b5eb8065
soomin <riato101@naver.com>
2026-09-14 11:13:03 +0900

    supabase에 staging용 프로젝트 만들고, 깃 저장소도 새로 만들어서 supabase 분기 만듦

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,14 @@
-const SUPABASE_URL = "https://brtxmfxieeoqczwzqwlq.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_kFXWDoFoNozQUkHdWYutQw_z2zOfPTS";
+const isStaging = location.href.includes('staging');
+
+const SUPABASE_URL = isStaging
+    ? "https://pqxkbcgljilebcpvgboj.supabase.co"
+    : "https://brtxmfxieeoqczwzqwlq.supabase.co";
+const SUPABASE_ANON_KEY = isStaging
+    ? "sb_publishable_10f-MtTz245wD1tglP5diQ_St4BqIIC"
+    :"sb_publishable_kFXWDoFoNozQUkHdWYutQw_z2zOfPTS";
 
 const { createClient } = supabase;
 const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+
+
